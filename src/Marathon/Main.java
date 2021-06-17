b32a023aa41dbc8cd494d2c50dd58e91f1dc2eeb
Alex <t1meal@yandex.ru>
2021-06-17 20:46:54 +0300

    домашка к 1 уроку Java lvl_2

--- src/Marathon/Main.java
+++ src/Marathon/Main.java
@@ -1,22 +1,19 @@
 package Marathon;
 
 import Marathon.Barrier.Course;
-import Marathon.Interfaceses.Competitor;
 import Marathon.MembersOfMarafon.Team;
 
 public class Main {
     public static void main(String[] args) {
-//        for (Competitor c : competitors) {
-//            for (Obstacle o : course) {
-//                o.doIt(c);
-//                if (!c.isOnDistance()) break;
-//            }
+
         Team team = new Team("Марафонцы");
         Course race = new Course("Первый забег", 200, 15, 10);
         race.startCourse(team);
         System.out.println();
+
         team.successfullyMembersInfo();
         System.out.println();
+
         team.allMembersInfo();
     }
 }
